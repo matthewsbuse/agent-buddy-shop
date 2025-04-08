900aa30180565f3c95f22291864f6ac30b8af6d8
matthewsbuse <matthewsbus@gmail.com>
2025-04-08 15:36:30 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/agent-buddy-shop/", // 👈 ADD THIS LINE
   server: {
     host: "::",
     port: 8080,
